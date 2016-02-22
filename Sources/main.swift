266e9094ec1a176fe83ce705e1fedcc9f2efc32c
Robert F. Dickerson <rfdickerson@gmail.com>
2016-02-21 19:15:05 -0600

    Put the training algorithm in a function

--- Sources/main.swift
+++ Sources/main.swift
@@ -4,10 +4,12 @@ import Foundation
 let threshold: Float = 0.5
 let learningRate: Float = 0.1
 
-var weights: [Float] = [0,0,0]
+
+
+typealias TrainingSet = [([Float],Float)]
 
 let trainingSet: [([Float],Float)] = [([1,0,0],1),([1,0,1],1),([1,1,0],1),([1,1,1],0)]
-
+//let trainingSet: TrainingSet = [([1,1,1],1),([1,0,0],0),([1,1,1],1),([0,0,1],1)]
 
 func dotProduct(values: [Float], _ weights: [Float]) -> Float {
 
@@ -15,28 +17,37 @@ func dotProduct(values: [Float], _ weights: [Float]) -> Float {
 
 }
 
-while true {
+func train( trainingSet: TrainingSet, _ threshold: Float = 0.5, _ learningRate: Float) -> [Float] {
+
+    var weights: [Float] = [0,0,0]
     
-    var errorCount: Int = 0
-    for (inputVector, desiredOutput) in trainingSet {
+    while true {
         
-        print(weights)
-        
-        let result = dotProduct(inputVector, weights) > threshold ? Float(1) : Float(0)
-        let error = desiredOutput - result
-        
-        if error != 0 {
-            errorCount += 1
-            for (index, value) in inputVector.enumerate() {
-                weights[index] += learningRate * error * value
+        var errorCount: Int = 0
+        for (inputVector, desiredOutput) in trainingSet {
+            
+            print(weights)
+            
+            let result = dotProduct(inputVector, weights) > threshold ? Float(1) : Float(0)
+            let error = desiredOutput - result
+            
+            if error != 0 {
+                errorCount += 1
+                for (index, value) in inputVector.enumerate() {
+                    weights[index] += learningRate * error * value
+                }
             }
+            
         }
         
+        if errorCount == 0 {
+            break
+        }
     }
-    
-    if errorCount == 0 {
-        break
-    }
+
+    return weights
+
 }
 
 
+train( trainingSet, threshold, learningRate )
